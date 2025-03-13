9ff25ab9bd9694e09d43b7ef08ef74899b769435
boddu shivasai <shivasai@mightyapps.ai>
2025-03-13 17:22:29 +0530

    Add/Update server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+ 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+let data = [];
+
+app.get('/data', (req, res) => {
+  res.json(data);
+});
+
+app.post('/data', (req, res) => {
+  data.push(req.body);
+  res.json(req.body);
+});
+
+app.put('/data/:id', (req, res) => {
+  const id = req.params.id;
+  const item = data.find(i => i.id === id);
+  if (!item) {
+    res.sendStatus(404);
+  } else {
+    Object.assign(item, req.body);
+    res.json(item);
+  }
+});
+
+app.delete('/data/:id', (req, res) => {
+  const id = req.params.id;
+  const index = data.findIndex(i => i.id === id);
+  if (index === -1) {
+    res.sendStatus(404);
+  } else {
+    data.splice(index, 1);
+    res.sendStatus(204);
+  }
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
